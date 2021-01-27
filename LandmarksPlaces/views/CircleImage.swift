3dc3662200529f7265fbc113372c476744241589
ghigbie <gehigbie@gmail.com>
2021-01-27 15:08:04 -0500

    Add new circle image

---
+++ LandmarksPlaces/views/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  LandmarksPlaces
+//
+//  Created by George Higbie on 1/27/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(borderColor, lineWidth: lineWidth))
+            .shadow(radius: radius)
+    }
+    
+    let lineWidth: CGFloat = 4
+    let radius: CGFloat = 10
+    let borderColor: Color = Color.white
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
